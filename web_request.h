e55f0e475d8d95b87ae6ede8192590f09e680a3c
Ege Bilecen <egebilecen@icloud.com>
2021-04-03 22:40:08 +0300

    Update web_request.h

--- web_request.h
+++ web_request.h
@@ -22,8 +22,8 @@ namespace EB
         typedef std::pair<std::string, std::string> PostData;
         typedef std::vector<PostData> PostField;
 
-        static long        last_http_code = -1;
-        static std::string useragent      = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36";
+        static long        _last_http_code = -1;
+        static std::string _useragent      = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36";
 
         size_t _write_data(void *ptr, size_t size, size_t nmemb, FILE *stream);
         size_t _write_string(void *ptr, size_t size, size_t nmemb, std::string* data);
@@ -34,5 +34,6 @@ namespace EB
         // post_field = "example=1&request=yes"
         bool send_post_request(std::string url, PostField post_field, std::string* response);
         bool send_get_request(std::string url, std::string* response);
+        long get_last_http_code();
     }
 }
