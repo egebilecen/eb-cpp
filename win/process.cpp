669a6fa30e01cbc4a67daf37f28200f9f7c0c336
Ege Bilecen <egebilecen@icloud.com>
2021-04-09 19:43:36 +0300

    Add files via upload

--- win/process.cpp
+++ win/process.cpp
@@ -2,124 +2,166 @@
 
 namespace EB
 {
-    // Private Method(s)
-    void Process::get_process_info(DWORD const* process_id, std::string const* process_name)
+    namespace Process
     {
-        if(process_id   == nullptr
-        && process_name == nullptr) return;
-
-        PROCESSENTRY32* process_info = new PROCESSENTRY32();
-        process_info->dwSize = sizeof(*process_info);
-
-        HANDLE processes_snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
-
-        if(processes_snapshot == INVALID_HANDLE_VALUE) return;
-
-        if(Process32First(processes_snapshot, process_info))
+        // Private Method(s)
+        void ExternalProcess::get_process_info(DWORD const* process_id, std::string const* process_name)
         {
-            do
-            {
-                if(process_id != nullptr)
-                {
-                    if(*process_id == process_info->th32ProcessID)
-                    {
-                        this->process_info = process_info;
-                        goto loop_end;
-                    }
-                }
-                else if(process_name != nullptr)
-                {
-                    std::wstring process_name_w = String::to_wstring(*process_name);
+            if(process_id   == nullptr
+               && process_name == nullptr) return;
 
-                    if(!process_name_w.compare(process_info->szExeFile))
+            PROCESSENTRY32* process_info = new PROCESSENTRY32();
+            process_info->dwSize = sizeof(*process_info);
+
+            HANDLE processes_snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+
+            if(processes_snapshot == INVALID_HANDLE_VALUE) return;
+
+            if(Process32First(processes_snapshot, process_info))
+            {
+                do
+                {
+                    if(process_id != nullptr)
                     {
-                        this->process_info = process_info;
-                        goto loop_end;
+                        if(*process_id == process_info->th32ProcessID)
+                        {
+                            this->process_info = process_info;
+                            goto loop_end;
+                        }
                     }
-                }
-            } 
-            while(Process32Next(processes_snapshot, process_info));
+                    else if(process_name != nullptr)
+                    {
+                        std::wstring process_name_w = String::to_wstring(*process_name);
+
+                        if(!process_name_w.compare(process_info->szExeFile))
+                        {
+                            this->process_info = process_info;
+                            goto loop_end;
+                        }
+                    }
+                } 
+                while(Process32Next(processes_snapshot, process_info));
+            }
+
+        loop_end:
+            CloseHandle(processes_snapshot);
+
+            if(this->process_info != nullptr)
+                this->process_handle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, *this->get_process_id());
+
+            return;
         }
 
-    loop_end:
-        CloseHandle(processes_snapshot);
-
-        if(this->process_info != nullptr)
-            this->process_handle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, *this->get_process_id());
-
-        return;
-    }
-
-    // Constructor(s)
-    Process::Process(std::string const& process_name)
-    {
-        this->get_process_info(nullptr, &process_name);
-    }
-
-    Process::Process(DWORD const& process_id)
-    {
-        this->get_process_info(&process_id, nullptr);
-    }
-
-    // Destructor
-    Process::~Process()
-    {
-        delete this->process_info;
-
-        if(this->process_handle != NULL)
-            CloseHandle(this->process_handle);
-    }
-
-    // Public Method(s)
-    PROCESSENTRY32 const* Process::get_process_info() const
-    {
-        return this->process_info;
-    }
-
-    DWORD const* Process::get_process_id() const
-    {
-        return this->process_info != nullptr ? &this->process_info->th32ProcessID : nullptr;
-    }
-
-    HANDLE const* Process::get_handle() const
-    {
-        return this->process_info != nullptr ? &this->process_handle : nullptr;
-    }
-
-    bool Process::load_module_list()
-    {
-        if(this->process_info == nullptr) return false;
-
-        MODULEENTRY32 module_info;
-        module_info.dwSize = sizeof(module_info);
-
-        HANDLE modules_snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, *this->get_process_id());
-
-        if(modules_snapshot == INVALID_HANDLE_VALUE) return false;
-
-        this->module_list.clear();
-
-        if(Module32First(modules_snapshot, &module_info))
+        // Constructor(s)
+        ExternalProcess::ExternalProcess(std::string const& process_name)
         {
-            do
-            {
-                ModuleInfo mod_info(std::wstring(module_info.szExePath), std::wstring(module_info.szModule),
-                                    module_info.modBaseAddr,             module_info.modBaseSize,
-                                    module_info.hModule);
-
-                this->module_list.push_back(mod_info);
-            } 
-            while(Module32Next(modules_snapshot, &module_info));
+            this->get_process_info(nullptr, &process_name);
         }
 
-    loop_end:
-        CloseHandle(modules_snapshot);
+        ExternalProcess::ExternalProcess(DWORD const& process_id)
+        {
+            this->get_process_info(&process_id, nullptr);
+        }
 
-        return true;
-    }
+        // Destructor
+        ExternalProcess::~ExternalProcess()
+        {
+            delete this->process_info;
 
-    std::vector<Process::ModuleInfo> const* Process::get_module_list() const
-    {
-        return &this->module_list;
+            if(this->process_handle != NULL)
+                CloseHandle(this->process_handle);
+        }
+
+        // Public Method(s)
+        PROCESSENTRY32 const* ExternalProcess::get_process_info() const
+        {
+            return this->process_info;
+        }
+
+        DWORD const* ExternalProcess::get_process_id() const
+        {
+            return this->process_info != nullptr ? &this->process_info->th32ProcessID : nullptr;
+        }
+
+        HANDLE const* ExternalProcess::get_handle() const
+        {
+            return this->process_info != nullptr ? &this->process_handle : nullptr;
+        }
+
+        bool ExternalProcess::load_module_list()
+        {
+            if(this->process_info == nullptr) return false;
+
+            MODULEENTRY32 module_info;
+            module_info.dwSize = sizeof(module_info);
+
+            HANDLE modules_snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, *this->get_process_id());
+
+            if(modules_snapshot == INVALID_HANDLE_VALUE) return false;
+
+            this->module_list.clear();
+
+            if(Module32First(modules_snapshot, &module_info))
+            {
+                do
+                {
+                    ModuleInfo mod_info(std::wstring(module_info.szExePath), std::wstring(module_info.szModule),
+                                        module_info.modBaseAddr,             module_info.modBaseSize,
+                                        module_info.hModule);
+
+                    this->module_list.push_back(mod_info);
+                } 
+                while(Module32Next(modules_snapshot, &module_info));
+            }
+
+            CloseHandle(modules_snapshot);
+            return true;
+        }
+
+        std::vector<Process::ModuleInfo> const* ExternalProcess::get_module_list() const
+        {
+            return &this->module_list;
+        }
+
+        namespace InternalProcess
+        {
+            DWORD get_process_id()
+            {
+                return GetCurrentProcessId();
+            }
+
+            HANDLE get_process_handle()
+            {
+                return GetCurrentProcess();
+            }
+
+            std::vector<ModuleInfo> get_module_list()
+            {
+                std::vector<ModuleInfo> mod_list;
+
+                MODULEENTRY32 module_info;
+                module_info.dwSize = sizeof(module_info);
+
+                HANDLE modules_snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, get_process_id());
+
+                if(modules_snapshot == INVALID_HANDLE_VALUE) return mod_list;
+
+                if(Module32First(modules_snapshot, &module_info))
+                {
+                    do
+                    {
+                        ModuleInfo mod_info(std::wstring(module_info.szExePath), std::wstring(module_info.szModule),
+                                            module_info.modBaseAddr,             module_info.modBaseSize,
+                                            module_info.hModule);
+
+                        mod_list.push_back(mod_info);
+                    } 
+                    while(Module32Next(modules_snapshot, &module_info));
+                }
+
+                CloseHandle(modules_snapshot);
+                return mod_list;
+            }
+        }
     }
 }
--- win/process.h
+++ win/process.h
@@ -10,10 +10,8 @@
 
 namespace EB
 {
-    class Process
+    namespace Process
     {
-    public:
-        // Structures
         struct ModuleInfo
         {
             std::wstring module_path;      // szExePath
@@ -26,34 +24,46 @@ namespace EB
                        BYTE*        module_base_addr, DWORD        module_base_size,
                        HMODULE      module_handle)
                 : module_path(module_path),           module_name(module_name),
-                  module_base_addr(module_base_addr), module_base_size(module_base_size),
-                  module_handle(module_handle)
+                module_base_addr(module_base_addr), module_base_size(module_base_size),
+                module_handle(module_handle)
             {}
         };
 
-    private:
-        // Variables
-        PROCESSENTRY32* process_info   = nullptr;
-        HANDLE          process_handle = NULL;
-        std::vector<ModuleInfo> module_list;
+        class ExternalProcess
+        {
 
-        // Method(s)
-        void get_process_info(DWORD const* process_id, std::string const* process_name);
-    
-    public:
-        // Constructor(s)
-        Process(std::string const& process_name);
-        Process(DWORD const& process_id);
+        private:
+            // Variables
+            PROCESSENTRY32* process_info   = nullptr;
+            HANDLE          process_handle = NULL;
+            std::vector<ModuleInfo> module_list;
 
-        // Destructor
-        ~Process();
+            // Method(s)
+            void get_process_info(DWORD const* process_id, std::string const* process_name);
 
-        // Method(s)
-        PROCESSENTRY32 const* get_process_info() const;
-        DWORD          const* get_process_id()   const;
-        HANDLE         const* get_handle()       const;
+        public:
+            // Constructor(s)
+            ExternalProcess(std::string const& process_name);
+            ExternalProcess(DWORD const& process_id);
 
-        bool load_module_list();
-        std::vector<ModuleInfo> const* get_module_list() const;
-    };
+            // Destructor
+            ~ExternalProcess();
+
+            // Method(s)
+            PROCESSENTRY32 const* get_process_info() const;
+            DWORD          const* get_process_id()   const;
+            HANDLE         const* get_handle()       const;
+
+            bool load_module_list();
+            std::vector<ModuleInfo> const* get_module_list() const;
+        };
+
+        namespace InternalProcess
+        {
+            DWORD get_process_id();
+            HANDLE get_process_handle();
+
+            std::vector<ModuleInfo> get_module_list();
+        }
+    }
 }
