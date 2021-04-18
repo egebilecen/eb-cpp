ede943b8ff0b62ef6406c4ed40d577b246d8cb98
Ege Bilecen <egebilecen@icloud.com>
2021-04-18 14:16:34 +0300

    Add files via upload

--- win/memory.cpp
+++ win/memory.cpp
@@ -6,6 +6,11 @@ namespace EB
     {
         namespace Memory
         {
+            void set_memory_chunk_size(size_t const& size)
+            {
+                _memory_chunk_size = size;
+            }
+            
             bool write_byte(HANDLE const& handle, DWORD const& addr, BYTE const& byte)
             {
                 return WriteProcessMemory(handle, (LPVOID)addr, &byte, 1, NULL);
@@ -31,6 +36,61 @@ namespace EB
             {
                 for(int i=0; i < size; i++)
                     write_byte(handle, addr + i, 0x90);
+            }
+
+            bool search_bytes(HANDLE const& handle, DWORD const& start_addr, DWORD const& end_addr, std::vector<BYTE> const& bytes, DWORD& addr_out, size_t const& nth)
+            {
+                DWORD current_addr = start_addr;
+
+                size_t chunk_size  = _memory_chunk_size;
+                BYTE* memory_chunk = new BYTE[chunk_size];
+                ZeroMemory(memory_chunk, chunk_size);
+
+                size_t found = 0;
+
+                while(current_addr >= start_addr
+                &&    current_addr <= end_addr)
+                {
+                    if(!ReadProcessMemory(handle, (LPVOID)current_addr, memory_chunk, chunk_size, NULL))
+                    {
+                        delete[] memory_chunk;
+                        return false;
+                    }
+
+                    // Check bytes
+                    for(size_t i=0; i <= chunk_size - sizeof(BYTE); i++)
+                    {
+                        size_t match_count = 0;
+
+                        if(bytes[0] == memory_chunk[i])
+                        {
+                            match_count += 1;
+
+                            for(size_t j=1; j < bytes.size(); j++)
+                            {
+                                if(bytes[j] == memory_chunk[i+j])
+                                    match_count += 1;
+                                else break;
+                            }
+
+                            if(match_count == bytes.size()
+                            && ++found == (nth+1))
+                            {
+                                addr_out = current_addr + i;
+
+                                delete[] memory_chunk;
+                                return true;
+                            }
+                        }
+                    }
+
+                    // Go to next chunk
+                    if(current_addr + chunk_size > end_addr) chunk_size = end_addr - current_addr;
+                    current_addr += chunk_size;
+                }
+
+                delete[] memory_chunk;
+                return false;
             }
         }
     }
--- win/memory.h
+++ win/memory.h
@@ -11,6 +11,10 @@ namespace EB
     {
         namespace Memory
         {
+            static size_t _memory_chunk_size = 0x00100000; // 1024kb
+
+            void set_memory_chunk_size(size_t const& size);
+
             bool write_byte(HANDLE const& handle, DWORD const& addr, BYTE const& byte);
             bool write(HANDLE const& handle, DWORD const& addr, std::vector<BYTE> const& bytes);
 
@@ -18,7 +22,7 @@ namespace EB
             bool read(HANDLE const& handle, BYTE* buffer, DWORD const& addr, size_t const& size);
 
             void fill_with_nop(HANDLE const& handle, DWORD const& addr, size_t const& size);
-            bool search_bytes(HANDLE const& handle, DWORD const& start_addr, DWORD const& end_addr, std::vector<BYTE> const& bytes);
+            bool search_bytes(HANDLE const& handle, DWORD const& start_addr, DWORD const& end_addr, std::vector<BYTE> const& bytes, DWORD& addr_out, size_t const& nth=0);
         }
     }
 }
