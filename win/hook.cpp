296606111bc3e36c11ac2f184da981717e0d6b3a
Ege Bilecen <egebilecen@icloud.com>
2021-04-29 10:36:34 +0300

    Add files via upload

--- win/hook.cpp
+++ win/hook.cpp
@@ -7,7 +7,7 @@ namespace EB
     {
         namespace Hook
         {
-        #ifndef _WIN64
+        #ifndef _WIN64 // x32
             bool relative_jmp(HANDLE const& handle, uintptr_t const& addr, LPVOID func, size_t const& size)
             {
                 constexpr size_t hook_size = 5;
@@ -24,6 +24,25 @@ namespace EB
 
                 if(size > hook_size) 
                     EB::Windows::Memory::fill_with_nop(handle, addr + hook_size, size - hook_size);
+
+                return true;
+            }
+
+            bool relative_jmp(uintptr_t const& addr, LPVOID func, size_t const& size)
+            {
+                constexpr size_t hook_size = 5;
+
+                if(size < hook_size) return false;
+
+                BYTE hook_bytes[hook_size] = { 0xe9, 0x00, 0x00, 0x00, 0x00 };
+
+                size_t relative_addr = (uintptr_t)func - (uintptr_t)addr - sizeof(hook_bytes);
+
+                memcpy(hook_bytes + 1, &relative_addr, 4);
+                EB::Windows::Memory::write(addr, hook_bytes, hook_size);
+
+                if(size > hook_size) 
+                    EB::Windows::Memory::fill_with_nop(addr + hook_size, size - hook_size);
 
                 return true;
             }
@@ -47,7 +66,7 @@ namespace EB
                     EB::Windows::Memory::fill_with_nop(handle, addr + hook_size, size - hook_size);
 
                 return true;
-            #else
+            #else // x32
                 constexpr size_t hook_size = 8;
 
                 if(size < hook_size) return false;
@@ -61,6 +80,41 @@ namespace EB
 
                 if(size > hook_size) 
                     EB::Windows::Memory::fill_with_nop(handle, addr + hook_size, size - hook_size);
+
+                return true;
+            #endif
+            }
+
+            bool absolute_jmp(uintptr_t const& addr, LPVOID func, size_t const& size)
+            {
+            #ifdef _WIN64
+                constexpr size_t hook_size = 13;
+
+                if(size < hook_size) return false;
+
+                BYTE hook_bytes[hook_size]  = { 0x49, 0xba, 0x88, 0x77, 0x66, 0x55, 0x44, 0x33, 0x22, 0x11, 0x41, 0xff, 0xe2 };
+                uintptr_t func_addr = (uintptr_t)func;
+
+                memcpy(hook_bytes + 2, &func_addr, sizeof(uintptr_t));
+                EB::Windows::Memory::write(addr, hook_bytes, hook_size);
+
+                if(size > hook_size) 
+                    EB::Windows::Memory::fill_with_nop(handle, addr + hook_size, size - hook_size);
+
+                return true;
+            #else // x32
+                constexpr size_t hook_size = 8;
+
+                if(size < hook_size) return false;
+
+                BYTE hook_bytes[hook_size]  = { 0x8d, 0x3d, 0x44, 0x33, 0x22, 0x11, 0xff, 0xe7 };
+                uintptr_t func_addr = (uintptr_t)func;
+
+                memcpy(hook_bytes + 2, &func_addr, sizeof(uintptr_t));
+                EB::Windows::Memory::write(addr, hook_bytes, hook_size);
+
+                if(size > hook_size) 
+                    EB::Windows::Memory::fill_with_nop(addr + hook_size, size - hook_size);
 
                 return true;
             #endif
--- win/hook.h
+++ win/hook.h
@@ -11,8 +11,10 @@ namespace EB
         {
         #ifndef _WIN64
             bool relative_jmp(HANDLE const& handle, uintptr_t const& addr, LPVOID func, size_t const& size);
+            bool relative_jmp(uintptr_t const& addr, LPVOID func, size_t const& size);
         #endif
             bool absolute_jmp(HANDLE const& handle, uintptr_t const& addr, LPVOID func, size_t const& size);
+            bool absolute_jmp(uintptr_t const& addr, LPVOID func, size_t const& size);
         }
     }
 }
